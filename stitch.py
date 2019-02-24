0769ac64e18bd9f3942ff332d2e3a3e91bc5a1a7
Adrian2333 <44152738+Adrian2333@users.noreply.github.com>
2019-02-24 15:53:59 -0500

    Delete stitch.py

--- stitch.py
+++
@@ -1,8 +0,0 @@
-import ocv, os.path as osp
-
-current_path = osp.abspath(osp.realpath("."))
-image_path = osp.join(current_path, r"To be Processed")
-
-cv = ocv.OCV(src_file_path=image_path)
-cv.stitch_images()
-cv.disp_images()
